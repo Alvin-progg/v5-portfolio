39d68f2be438ee681af39c03b98d75051aa02175
ALvin-progg <doctoralvim726@gmail.com>
2025-08-30 17:52:46 +0800

    wdqq1

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,20 +4,20 @@ import React from "react";
 export default function Nav(){
     return(
 
-        <nav className="w-full h-10">
-            <div className="flex justify-between p-7 mx-66">
-                <a href="#" className="font-bold text-3xl">
+        <nav className="w-full h-[10vh] ">
+            <div className="flex justify-between p-7 mx-66 py-10 ">
+                <a href="#" className="font-bold text-3xl text-secondary-800 hover:text-primary-600">
                     VIN.
                 </a>
-                <ul className="flex gap-7 text-2xl font-semibold">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Experience</a></li>
-                    <li><a href="#">Projects</a></li>
-                    <li><a href="#">Faq</a></li>
+                <ul className="flex gap-10 text-2xl font-semibold items-center">
+                    <li><a href="#" className="text-secondary-800 hover:text-primary-600">Home</a></li>
+                    <li><a href="#" className="text-secondary-800 hover:text-primary-600">About</a></li>
+                    <li><a href="#" className="text-secondary-800 hover:text-primary-600">Experience</a></li>
+                    <li><a href="#" className="text-secondary-800 hover:text-primary-600">Projects</a></li>
+                    <li><a href="#" className="text-secondary-800 hover:text-primary-600">Faq</a></li>
                 </ul>
                 <div>
-                    <a href="#"  className="text-3xl font-semibold ">
+                    <a href="#"  className="text-2xl font-semibold text-secondary-200  bg-slate-900 p-2 px-4 rounded-2xl">
                         Lets talk
                     </a>
                 </div>
