e1e8f20e1ad37f45069a771380d6f1c653c54010
ALvin-progg <doctoralvin726@gmail.com>
2025-08-31 01:34:55 +0800

    12d1wq1

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -1,7 +1,7 @@
 export default function Experience(){
     return(
         <section id="experience" className="w-full ">
-            <div className="flex flex-col  py-10 m-2">
+            <div className="flex flex-col  mt-20 m-2">
                 <div className="flex flex-col gap-3 items-center">
                     <p className="font-semibold text-primary-700 text-base">My Career</p>
                     <h2 className="text-3xl font-bold">Experience</h2>
@@ -13,7 +13,7 @@ export default function Experience(){
                             <p>WeTech Dev ( Remote )</p>
                             <p>(2025 - Present )</p>
                         </div>
-                        <p className="text-xl tracking-wide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lisque persius interesset his et ad mea essent possim iriure.</p>
+                        <p className="text-xl tracking-wider font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lisque persius interesset his et ad mea essent possim iriure.</p>
                     </div>
                     <div className="w-[900px] max-lg:w-auto flex flex-col gap-3.5 border-b py-5 border-secondary-300">
                         <h3 className="text-3xl font-bold">Technical Support</h3>
@@ -21,11 +21,15 @@ export default function Experience(){
                             <p>Batstate-u </p>
                             <p>(2024 - Present )</p>
                         </div>
-                        <p className="text-xl tracking-wide">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lisque persius interesset his et ad mea essent possim iriure.</p>
+                        <p className="text-xl tracking-wider font-light">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lisque persius interesset his et ad mea essent possim iriure.</p>
                     </div>
-
                 </div>
-
+            </div>
+            <div className="flex flex-col items-center">
+                <div className="flex flex-col gap-3 items-center">
+                    <p className="font-semibold text-primary-700 text-base">Expertise</p>
+                    <h2 className="text-3xl font-bold">Tech Stack</h2>
+                </div>
             </div>
             
         </section>
