cb59c6007a937d99cabae3777275185d6b79b8ba
ALvin-progg <doctoralvin726@gmail.com>
2025-09-03 13:34:09 +0800

    q21gf

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,12 +9,12 @@ export default function Project() {
         <p className="font-semibold text-primary-700 text-base">My Work</p>
         <h2 className="text-3xl font-bold">Projects</h2>
       </div>
-      <div className="grid max-lg:grid-cols-1 grid-cols-2 items-center justify-center my-10 py-5 mx-66 max-lg:mx-0 gap-4">
+      <div className="grid max-lg:grid-cols-1 grid-cols-2 items-center justify-center my-10 py-5 mx-66 max-lg:mx-0 gap-4 max-xl:gap-44">
         {projectConst.map((project) => {
           return (
             <div
               key={project.id}
-              className="flex flex-col w-[600px] gap-3 max-sm:w-auto border h-[400px] border-secondary-300 rounded-2xl p-4 items-center "
+              className="flex flex-col w-[600px] gap-3 max-sm:w-auto border h-[400px] border-secondary-300 rounded-2xl p-4 items-center  max-xl:mx-0 max-xl:w-[300px]"
             >
               <Image
                 loading="lazy"
@@ -22,11 +22,11 @@ export default function Project() {
                 alt={`${project.name} image`}
                 width={250}
                 height={250}
-                className="object-cover rounded-2xl"
+                className="object-cover rounded-2xl h-auto w-auto"
               />
               <div className="flex flex-col gap-3.5">
                 <h2 className="text-3xl text-primary-600 font-semibold">{project.name}</h2>
-                <p className="leading-loose font-light">{project.disc}</p>
+                <p className="leading-loose font-light max-xl:text-sm max-xl:leading-none">{project.disc}</p>
               </div>
             </div>
           );
