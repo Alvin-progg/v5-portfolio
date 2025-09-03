0904a6e07cf4d505c6fabab14afb0071bdd5f25c
ALvin-progg <doctoralvin726@gmail.com>
2025-09-03 13:30:38 +0800

    1d1fv1

--- src/components/Project.js
+++ src/components/Project.js
@@ -9,24 +9,24 @@ export default function Project() {
         <p className="font-semibold text-primary-700 text-base">My Work</p>
         <h2 className="text-3xl font-bold">Projects</h2>
       </div>
-      <div className="grid grid-cols-2 items-center justify-center my-10 py-5 mx-66 gap-4">
+      <div className="grid max-lg:grid-cols-1 grid-cols-2 items-center justify-center my-10 py-5 mx-66 max-lg:mx-0 gap-4">
         {projectConst.map((project) => {
           return (
             <div
               key={project.id}
-              className="flex flex-col w-[600px] border border-secondary-300 rounded-2xl p-4 items-center "
+              className="flex flex-col w-[600px] gap-3 max-sm:w-auto border h-[400px] border-secondary-300 rounded-2xl p-4 items-center "
             >
               <Image
                 loading="lazy"
                 src={project.img}
                 alt={`${project.name} image`}
-                width={200}
-                height={200}
+                width={250}
+                height={250}
                 className="object-cover rounded-2xl"
               />
               <div className="flex flex-col gap-3.5">
-                <h2 className="text-2xl text-primary-600 font-semibold">{project.name}</h2>
-                <p>{project.disc}</p>
+                <h2 className="text-3xl text-primary-600 font-semibold">{project.name}</h2>
+                <p className="leading-loose font-light">{project.disc}</p>
               </div>
             </div>
           );
--- src/constants/projectConst.js
+++ src/constants/projectConst.js
@@ -1,45 +1,60 @@
-import test from "@/../public/test.jpg"
+import test from "@/../public/test.jpg";
+
+import p1 from "@/../public/project/p1.png";
+import p2 from "@/../public/project/p2.png";
+import p3 from "@/../public/project/p3.png";
+import p4 from "@/../public/project/p4.png";
+import p5 from "@/../public/project/p5.png";
+import p6 from "@/../public/project/p6.png";
 
 export const projectConst = [
   {
     id: 1,
     name: "Project 1",
     disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    img: test,
+    img: p1,
     source: "https://github.com/Alvin-progg",
-    demo: "https://alvin-prog-v5.vercel.app/"
+    demo: "https://alvin-prog-v5.vercel.app/",
   },
-    {
+  {
     id: 2,
     name: "Project 2",
     disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    img: test,
+    img: p2,
     source: "https://github.com/Alvin-progg",
-    demo: "https://alvin-prog-v5.vercel.app/"
+    demo: "https://alvin-prog-v5.vercel.app/",
   },
-    {
+  {
     id: 3,
     name: "Project 3",
     disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    img: test,
+    img: p3,
     source: "https://github.com/Alvin-progg",
-    demo: "https://alvin-prog-v5.vercel.app/"
+    demo: "https://alvin-prog-v5.vercel.app/",
   },
-    {
+  {
     id: 4,
     name: "Project 4",
     disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    img: test,
+    img: p4,
     source: "https://github.com/Alvin-progg",
-    demo: "https://alvin-prog-v5.vercel.app/"
+    demo: "https://alvin-prog-v5.vercel.app/",
   },
-    {
+  {
     id: 5,
     name: "Project 5",
     disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    img: test,
+    img: p5,
     source: "https://github.com/Alvin-progg",
-    demo: "https://alvin-prog-v5.vercel.app/"
+    demo: "https://alvin-prog-v5.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "Project 5",
+    disc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    img: p6,
+    source: "https://github.com/Alvin-progg",
+    demo: "https://alvin-prog-v5.vercel.app/",
   },
 ];
 
