5f43a7fb908daeb78ae2d76038280b6470769a2a
ALvin-progg <doctoralvin726@gmail.com>
2025-09-22 19:20:38 +0800

    added a project

--- src/constants/projectConst.js
+++ src/constants/projectConst.js
@@ -4,6 +4,7 @@ import p3 from "@/../public/project/p3.png";
 import p4 from "@/../public/project/p4.png";
 import p5 from "@/../public/project/p5.png";
 import p6 from "@/../public/project/p6.png";
+import p7 from "@/../public/project/p7.png";
 
 export const projectConst = [
   {
@@ -15,34 +16,41 @@ export const projectConst = [
   },
   {
     id: 2,
+    name: "Ordering and Reservation System with Admin Panel",
+    disc: "An Order and Reservation System built with HTML, Tailwind CSS, and JavaScript for the frontend, connected to a PHP and MySQL backend API. It handles customer reservations, order management, and user records, providing a simple and efficient way to streamline booking and ordering processes.",
+    img: p7,
+    link: "https://github.com/Alvin-progg/JOSEPHINE-S-BACKYARD-STEAKHOUSE"
+  },
+  {
+    id: 3,
     name: "Tobe Nguyen",
     disc: "A Discord bot built with Node.js designed to manage raid events. It automates raid sign-ups, handles raid scheduling, and was mainly used for AVA raids.",
     img: p2,
     link: "https://github.com/Alvin-progg/Tobe_bot",
   },
   {
-    id: 3,
+    id: 4,
     name: "Portfolio v3",
     disc: "An earlier version of my personal portfolio built with React and Tailwind CSS. It showcases my projects and skills but has since been improved in Portfolio v4.",
     img: p3,
     link: "https://github.com/Alvin-progg/main-portfolio",
   },
   {
-    id: 4,
+    id: 5,
     name: "EduCore C++ System",
     disc: "A C++ console-based academic management system that manages courses, students, grades, and GWA reports using the Philippine grading system.",
     img: p4,
     link: "https://github.com/Alvin-progg/EduCore",
   },
   {
-    id: 5,
+    id: 6,
     name: "Banking C++ System",
     disc: "A C++ console-based banking management system that supports account creation, deposits, withdrawals, balance inquiries, and transaction history tracking.",
     img: p5,
     link: "https://github.com/Alvin-progg/banking-system",
   },
   {
-    id: 6,
+    id: 7,
     name: "Proposal Landing Page",
     disc: "A simple landing page built for a company proposal. It was developed as a free project to present information in a clean and professional way.",
     img: p6,
